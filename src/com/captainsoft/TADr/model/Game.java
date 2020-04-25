21e819800d72e589183dc5de549aa830e4b0ae55
Mathias Fringes <mathias.fringes@googlemail.com>
2020-04-25 09:35:50 +0200

    tabs to spaces

--- src/com/captainsoft/TADr/model/Game.java
+++ src/com/captainsoft/TADr/model/Game.java
@@ -16,78 +16,78 @@ import static com.captainsoft.spark.utils.Utils.stringer;
  * @author mathias fringes
  */
 public final class Game {
-	
-	// fields
-	
-	public static final int NEW_GAME_NR = -1;
+
+    // fields
+
+    public static final int NEW_GAME_NR = -1;
 
     private final Party party;
     private final PartyQuestLog questLog = new PartyQuestLog();
 
-	private int number = NEW_GAME_NR;
-	private LevelMap levelMap;
-	private String name;
-	private String player;
+    private int number = NEW_GAME_NR;
+    private LevelMap levelMap;
+    private String name;
+    private String player;
 
-	// constructor
-	
-	public Game(String playerName, Party party) {
-		super();
+    // constructor
+
+    public Game(String playerName, Party party) {
+        super();
         this.player(playerName);
         this.party = party;
-	}
+    }
 
-	// accessors
-	
-	public LevelMap LevelMap() {		
-		return levelMap;
-	}
-	
-	public void LevelMap(LevelMap levelMap) {		
-		this.levelMap = levelMap;
-	}
+    // accessors
+
+    public LevelMap LevelMap() {
+        return levelMap;
+    }
+
+    public void LevelMap(LevelMap levelMap) {
+        this.levelMap = levelMap;
+    }
 
     public PartyQuestLog QuestLog() {
         return questLog;
     }
-	
-	public String name() {
-		return name;
-	}
 
-	public void name(String name) {
-		this.name = name;
-	}
+    public String name() {
+        return name;
+    }
 
-	public void number(int number) {
-		this.number = number; 		
-	}
+    public void name(String name) {
+        this.name = name;
+    }
 
-	public int number() {
-		return number;
-	}
-	
-	public boolean isNewGame() {
-		return (number == NEW_GAME_NR);
-	}
-	
-	public Party party() {
-		return party;
-	}
+    public void number(int number) {
+        this.number = number;
+    }
 
-	public void player(String player) {
-		this.player = player;
-	}
+    public int number() {
+        return number;
+    }
 
-	public String player() {
-		return player;
-	}
-	
-	// overridden
-	
-	@Override
-	public String toString() {	
-		return stringer("Game", number, name);
-	}
+    public boolean isNewGame() {
+        return (number == NEW_GAME_NR);
+    }
+
+    public Party party() {
+        return party;
+    }
+
+    public void player(String player) {
+        this.player = player;
+    }
+
+    public String player() {
+        return player;
+    }
+
+    // overridden
+
+    @Override
+    public String toString() {
+        return stringer("Game", number, name);
+    }
 
 }
